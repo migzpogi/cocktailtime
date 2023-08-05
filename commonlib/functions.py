d5657289b6309c76cf414aef78160df84dea68ee
migzpogi <migz.zgim@gmail.com>
2023-08-05 11:47:20 +0200

    Adds commonlib/func

---
+++ commonlib/functions.py
@@ -0,0 +1,10 @@
+def sum_func(a, b):
+    """
+    Returns the sum of a and b
+    A placeholder function for unit testing
+    :param int a: first integer
+    :param int b: second integer
+    :return int: sum of a and b
+    """
+
+    return a+b
