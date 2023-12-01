b7eda598f9c0a02a92349654ff88be0bd2ec4994
migzpogi <migz.zgim@gmail.com>
2023-12-02 00:24:20 +0100

    Adds basic yaml parsing

--- yamlparsing/app.py
+++ yamlparsing/app.py
@@ -1,5 +1,35 @@
+import yaml
+
+
+def open_yaml_to_dict(file_path):
+    """
+    Opens the YAML file and converts it to Python dictionary
+    :param str file_path: Path of the YAML file
+    :return dict: Python dictionary form of the YAML file
+    """
+    with open(file_path, 'r') as file:
+        yaml_file_as_dict = yaml.safe_load(file)
+
+    return yaml_file_as_dict
+
+
+def get_extends_template(yaml_dict):
+    """
+    Gets the "template" value from the "extends" key
+    :param dict yaml_dict: YAML file in dictionary format
+    :return:
+    """
+
+    return yaml_dict.get("extends").get("template")
+
+
 def foobar():
     return 1
 
+
 def flask_landing():
     return "yamlparsing landing"
+
+
+y = open_yaml_to_dict("sample.yaml")
+print(get_extends_template(y))
