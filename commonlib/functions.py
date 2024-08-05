ee5a9a084e0bef59b70a9af60148fad2be070d8a
migzpogi <migz.zgim@gmail.com>
2024-08-05 11:11:42 +0200

    Adds is_date8600

--- commonlib/functions.py
+++ commonlib/functions.py
@@ -13,3 +13,13 @@ def sum_func(a, b):
 def easy_time(yyyy_mm_dd):
     rfc3399 = f"{yyyy_mm_dd}T00:00:00+0000"
     return rfc3399
+
+
+def is_datestring_iso8600(datestring):
+    """
+    Checks if the provided date (string) follows the YYYY-MM-DD format (ISO8600)
+    :param str datestring: Date in string format
+    :return boolean: Is datestring following ISO8600 format
+    """
+
+    return True
--- tests/test_commonlib.py
+++ tests/test_commonlib.py
@@ -1,10 +1,13 @@
 import unittest
-from commonlib.functions import sum_func
+from commonlib.functions import sum_func, is_datestring_iso8600
 
 
 class TestCommonLib(unittest.TestCase):
 
     def test_sum(self):
+        self.assertTrue(sum_func(1, 2), 3)
+
+    def test_datestring(self):
         self.assertTrue(sum_func(1, 2), 3)
 
 
